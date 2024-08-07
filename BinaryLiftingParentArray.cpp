bac8b2c469bbd4fffd85bfe76761e5842852a99f
Rukmaiwad Dattatri <106620566+Rukmaiwad@users.noreply.github.com>
2024-08-08 00:29:18 +0530

    Update BinaryLiftingParentArray.cpp

--- BinaryLiftingParentArray.cpp
+++ BinaryLiftingParentArray.cpp
@@ -7,15 +7,22 @@ class BinaryLifting
 public:
     vector<vector<ll>> anc;
     int maxLog;
+    vector<int> levelsArray;
+    vector<vector<ll>> adj;
 
     BinaryLifting(int n, vector<int> &parArray)
     {
         maxLog = 21; // Sufficient for n up to 2^21 - 1
         anc.resize(n + 1, vector<ll>(maxLog, -1));
+        adj.resize(n + 1, vector<ll>());
+        levelsArray = vector<int>(n + 1, 0);
 
         // Initialize the first ancestor (2^0 = 1 step) from parArray
         for (int i = 2; i <= n; i++)
         {
+            // Creating adjacency list
+            adj[i].push_back(parArray[i - 2]);
+            adj[parArray[i - 2]].push_back(i);
             anc[i][0] = parArray[i - 2];
         }
 
@@ -32,7 +39,7 @@ public:
         }
     }
 
-    // function to find kth ancestor and node u
+    // Function to find kth ancestor and node u
     ll kanc(ll u, ll k)
     {
         if (u < 1)
@@ -50,25 +57,73 @@ public:
         }
         return u;
     }
+
+    void levels(int cur, int par)
+    {
+        levelsArray[cur] = levelsArray[par] + 1;
+        for (auto ch : adj[cur])
+        {
+            if (ch != par)
+            {
+                levels(ch, cur);
+            }
+        }
+    }
+
+    ll lca(int u, int v)
+    {
+        if (levelsArray[u] < levelsArray[v])
+        {
+            swap(u, v);
+        }
+
+        ll diff = levelsArray[u] - levelsArray[v];
+
+        for (ll j = 20; j >= 0; j--)
+        {
+            if ((1 << j) & diff)
+            {
+                u = anc[u][j];
+            }
+        }
+
+        if (u == v)
+        {
+            return u;
+        }
+
+        for (ll j = 20; j >= 0; j--)
+        {
+            if (anc[u][j] != anc[v][j])
+            {
+                u = anc[u][j];
+                v = anc[v][j];
+            }
+        }
+        return anc[u][0];
+    }
 };
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t-- > 0)
     {
-        int n, q; // read size of tree and number of queries
+        int n, q; // Read size of tree and number of queries
         cin >> n >> q;
 
-        // read input as parent array
+        // Read input as parent array
         vector<int> parA(n - 1);
         for (int i = 0; i < n - 1; i++)
         {
             cin >> parA[i];
         }
 
-        // read q number of queries
+        // Read q number of queries
         vector<vector<int>> queries(q, vector<int>(2));
         for (int i = 0; i < q; i++)
         {
@@ -76,12 +131,15 @@ int main()
         }
 
         BinaryLifting bl(parA.size() + 1, parA); // Adjust size for 1-based indexing
-        vector<int> ans;
+
+        bl.levels(1, 0); // Initialize levels from root node
+
         for (auto &query : queries)
         {
             int u = query[0];
             int k = query[1];
-            int res = bl.kanc(u, k);
+            // int res = bl.kanc(u,k);
+            int res = bl.lca(u, k);
             cout << res << endl;
         }
     }
