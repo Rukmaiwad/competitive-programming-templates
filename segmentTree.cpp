1ab5ee779e729522d7f881f0f6f969f4e7c3b92c
Rukmaiwad Dattatri <106620566+Rukmaiwad@users.noreply.github.com>
2024-08-10 23:21:15 +0530

    Update segmentTree.cpp
    updated segment tree template

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -1,101 +1,95 @@
-#include <vector>
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 
-class SegmentTree
-{
+class SegmentTree {
 public:
-    vector<ll> tree;
-    int n;
+    vector<ll> seg;
 
-    SegmentTree(int size)
-    {
-        n = size;
-        tree.resize(4 * n, 0);
+    SegmentTree(ll n) {
+        seg.resize(4 * n + 1);
     }
 
-    void build(vector<ll> &arr, int v, int tl, int tr)
-    {
-        if (tl == tr)
-        {
-            tree[v] = arr[tl];
+    void build(ll ind, ll low, ll high, ll arr[]) {
+        if (low == high) {
+            seg[ind] = arr[low];
+            return;
         }
-        else
-        {
-            int tm = (tl + tr) / 2;
-            build(arr, v * 2, tl, tm);
-            build(arr, v * 2 + 1, tm + 1, tr);
-            tree[v] = tree[v * 2] + tree[v * 2 + 1];
-        }
+        ll mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
 
-    ll sum(int v, int tl, int tr, int l, int r)
-    {
-        if (l > r)
-            return 0;
-        if (l == tl && r == tr)
-            return tree[v];
-        int tm = (tl + tr) / 2;
-        return sum(v * 2, tl, tm, l, min(r, tm)) + sum(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
+    ll query(ll ind, ll low, ll high, ll l, ll r) {
+        // no overlap
+        if (r < low || high < l) return LLONG_MAX;
+
+        // complete overlap
+        if (low >= l && high <= r) return seg[ind];
+
+        // partial overlap
+        ll mid = (low + high) >> 1;
+        ll left = query(2 * ind + 1, low, mid, l, r);
+        ll right = query(2 * ind + 2, mid + 1, high, l, r);
+        return min(left, right);
     }
 
-    void update(int v, int tl, int tr, int pos, ll new_val)
-    {
-        if (tl == tr)
-        {
-            tree[v] = new_val;
+    void update(ll ind, ll low, ll high, ll i, ll val) {
+        if (low == high) {
+            seg[ind] = val;
+            return;
         }
-        else
-        {
-            int tm = (tl + tr) / 2;
-            if (pos <= tm)
-            {
-                update(v * 2, tl, tm, pos, new_val);
-            }
-            else
-            {
-                update(v * 2 + 1, tm + 1, tr, pos, new_val);
-            }
-            tree[v] = tree[v * 2] + tree[v * 2 + 1];
-        }
+        ll mid = (low + high) >> 1;
+        if (i <= mid) update(2 * ind + 1, low, mid, i, val);
+        else update(2 * ind + 2, mid + 1, high, i, val);
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
 };
 
-int main()
-{
-    int n;
-    cin >> n;
-    vector<ll> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+void solve2() {
+    // if(two arrays are given as input create 2 instance of class)
+    ll n1;
+    cin >> n1;
+    ll arr1[n1];
+    for (ll i = 0; i < n1; i++) cin >> arr1[i];
+    SegmentTree sg1(n1);
+    sg1.build(0, 0, n1 - 1, arr1);
 
-    SegmentTree st(n);
-    st.build(arr, 1, 0, n - 1);
+    ll n2;
+    cin >> n2;
+    ll arr2[n2];
+    for (ll i = 0; i < n2; i++) cin >> arr2[i];
+    SegmentTree sg2(n2);
+    sg2.build(0, 0, n2 - 1, arr2);
 
-    // Example usage
-    int q;
+    ll q;
     cin >> q;
-    while (q--)
-    {
-        int type;
+    while (q--) {
+        ll type;
         cin >> type;
-        if (type == 1)
-        {
-            int l, r;
-            cin >> l >> r;
-            cout << st.sum(1, 0, n - 1, l, r) << endl;
-        }
-        else if (type == 2)
-        {
-            int pos;
-            ll new_val;
-            cin >> pos >> new_val;
-            st.update(1, 0, n - 1, pos, new_val);
+        if (type == 1) {
+            ll l1, r1, l2, r2;
+            cin >> l1 >> r1 >> l2 >> r2;
+            ll min1 = sg1.query(0, 0, n1 - 1, l1, r1);
+            ll min2 = sg2.query(0, 0, n2 - 1, l2, r2);
+            cout << min(min1, min2) << endl;
+        } else {
+            ll arrNo, i, val;
+            cin >> arrNo >> i >> val;
+            if (arrNo == 1) {
+                sg1.update(0, 0, n1 - 1, i, val);
+                arr1[i] = val;
+            } else {
+                sg2.update(0, 0, n2 - 1, i, val);
+                arr2[i] = val;
+            }
         }
     }
+}
+
+int main() {
+    solve2();
     return 0;
 }
